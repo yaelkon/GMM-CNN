b69f4f36c1f13dd7c7e2c620dbef9f1df36a6eca
yaelkon <yaelkon3@gmail.com>
2021-09-20 17:09:09 +0200

    main suitable for watermark experiment

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import os
 import numpy as np
+import time
+import argparse
 from os.path import join as pjoin
 from gmm_cnn import GMM_CNN
+from utils.data_preprocessing import add_watermark
 from keras.datasets import cifar10
 from keras.utils import to_categorical
+from utils.vis_utils import get_cifar10_labels, get_cifar10_watermarks_dict
+from utils.data_preprocessing import add_watermark_by_class
+
+parser = argparse.ArgumentParser(description='Watermark experiment')
+
+parser.add_argument('--class1', dest='cls1',
+                    help='The name of the first class to add watermarks on top of its images',
+                    required=True, type=str)
+parser.add_argument('--class2', dest='cls2',
+                    help='The name of the second class to add watermarks on top of its images',
+                    required=True, type=str)
+
+args = parser.parse_args()
 
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
 """ 
@@ -11,7 +27,7 @@ An experiment pip for modeling CNN layers with GMM.
 Define your experiment parameters below
 """
 
-SAVING_DIR = pjoin(*['C:', os.environ["HOMEPATH"], 'Desktop', 'tmp', 'resnet20_cifar10'])
+SAVING_DIR = pjoin(*['C:', os.environ["HOME"], 'Desktop', 'tmp', 'resnet20_cifar10', time.strftime('%Y%m%d_%H%M%S')])
 
 # A pre-trained network's weights - optional
 WEIGHTS_DIR = pjoin(os.path.abspath(os.getcwd()), 'utils', 'cifar10_resnet20_weights.97.hdf5')
@@ -37,13 +53,32 @@ num_epochs = 10
 
 
 # -----------------------   Prepare cifar 10 dataset    --------------------------
-
 (x_train, y_train), (x_val, y_val) = cifar10.load_data()
+
+labels = get_cifar10_labels()
+base_watermarks_dict = get_cifar10_watermarks_dict()
+WM_dict = {args.cls1: base_watermarks_dict[args.cls1], args.cls2: base_watermarks_dict[args.cls2]}
+
+print('Preparing train watermark dataset')
+x_train = add_watermark_by_class(WM_dict,
+                                 x_train,
+                                 y_train,
+                                 labels,
+                                 train0validation1=0,
+                                 save_dataset=True,
+                                 saveing_dir=SAVING_DIR)
+print('Preparing validation watermark dataset')
+x_val = add_watermark_by_class(WM_dict,
+                               x_val,
+                               y_val,
+                               labels,
+                               train0validation1=1,
+                               save_dataset=True,
+                               saveing_dir=SAVING_DIR)
 
 # Convert class vectors to binary class matrices.
 y_train = to_categorical(y_train, 10)
 y_val = to_categorical(y_val, 10)
-
 # Normalize the data
 x_train = x_train.astype('float32')
 x_val = x_val.astype('float32')
@@ -54,6 +89,7 @@ x_val /= 255
 x_train_mean = np.mean(x_train, axis=0)
 x_train -= x_train_mean
 x_val -= x_train_mean
+
 
 # ------------------------   Begin Training  -------------------------------------
 
@@ -72,7 +108,7 @@ model.compile_model()
 
 print('Initialising GMM parameters')
 
-layers_gmm_params = model.calc_modeled_layers_mean_and_std(x_train[:1000])
+layers_gmm_params = model.calc_modeled_layers_mean_and_std(x_train[:100])
 model.set_weights(layers_gmm_params)
 
 # Fit the labels size according to the number of outputs layers
