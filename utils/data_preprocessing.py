ee901ea6072541aa6417d0267415d4608e18094e
yaelkon <yaelkon3@gmail.com>
2021-09-20 17:09:47 +0200

    adding watermarks on train and val data and save them

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -1,14 +1,35 @@
+from os.path import join as pjoin
 import matplotlib.pyplot as plt
 import random
 import numpy as np
 from PIL import Image, ImageFont, ImageDraw
+from utils.file import makedir
 
 
-def add_watermark(image, text='T', fontsize=10, color=(0, 0, 0), offX=None, offY=None):
+def pure_pil_alpha_to_color(image, color=(255, 255, 255)):
+    """Alpha composite an RGBA Image with a specified color.
+
+    Simpler, faster version than the solutions above.
+
+    Source: http://stackoverflow.com/a/9459208/284318
+
+    Keyword Arguments:
+    image -- PIL RGBA Image object
+    color -- Tuple r, g, b (default 255, 255, 255)
+
+    """
+    image.load()  # needed for split()
+    background = Image.new('RGB', image.size, color)
+    background.paste(image, mask=image.split()[3])  # 3 is the alpha channel
+    return background
+
+
+def add_watermark(image, text='T', fontsize=8, color=(255, 0, 0, 100), offX=None, offY=None, verbose=0):
     img_width, img_height, _ = image.shape
 
-    image = Image.fromarray(np.uint8(image))
-    draw = ImageDraw.Draw(image)
+    pil_image = Image.fromarray(np.uint8(image))
+    pil_image = pil_image.convert('RGBA')
+    draw = ImageDraw.Draw(pil_image)
 
     font = ImageFont.truetype("Ubuntu-R.ttf", fontsize)
 
@@ -19,25 +40,39 @@ def add_watermark(image, text='T', fontsize=10, color=(0, 0, 0), offX=None, offY
     # add watermark
     # If the image size is smaller than the watermark image, no watermark is added
     if img_width >= fontsize and img_height >= fontsize:
+
         x = int((img_width - fontsize)*offX)
         y = int((img_height - fontsize)*offY)
         draw.text((x, y), text, fill=color, font=font)
-        plt.title("black text")
-        plt.imshow(image)
 
-    return np.array(image)
+        # Keep transparent data
+        # rgb_img = pil_image.convert('RGB')
+        rgb_img = pure_pil_alpha_to_color(pil_image, color=color[:-1])
+        if verbose:
+            plt.subplots()
+            plt.subplot(1, 2, 1)
+            plt.title("RGBA")
+            plt.imshow(pil_image)
+            plt.subplot(1, 2, 2)
+            plt.title("RGB")
+            plt.imshow(rgb_img)
+
+        return np.array(rgb_img)
 
 
-def add_watermark_by_class(class_watermark_dict, X, Y, labels_str, save_dataset=True, saveing_dir='/tmp/'):
+def add_watermark_by_class(class_watermark_dict, X, Y, labels_str, train0validation1=0,
+                           save_dataset=False, saveing_dir='/tmp/'):
     """
+    :param save_dataset:
     :param class_watermark_dict: {class_name: wm_symbol} -> {str: str}
     :param X: data
     :param Y: labels
     :param labels_str: list of labels str
-    :return: x_watermark
+    :return: X_watermark
     """
     X_watermark = np.empty_like(X)
     n_data = X.shape[0]
+    n_digits = '0' + str(len(str(n_data)))
 
     for i, (x, y) in enumerate(zip(X, Y)):
         y = y[0]
@@ -48,6 +83,14 @@ def add_watermark_by_class(class_watermark_dict, X, Y, labels_str, save_dataset=
                               )
         X_watermark[i] = x
 
-        # if save_dataset:
+        if save_dataset:
+            if i == 0:
+                # Create dataset root
+                saveing_dir = pjoin(saveing_dir, 'Watermark_Data', 'validation' if train0validation1 else 'train')
+                makedir(saveing_dir)
+            # Save image
+            pil_img = Image.fromarray(x)
+            full_dir = pjoin(saveing_dir, f'{i:{n_digits}}' + '.png')
+            pil_img.save(full_dir)
 
     return X_watermark
